6119c9afe5596b8e4bb7d204a64c3b2a0fb378e0
KratiGupta <kratigupta8545@gmail.com>
2021-08-07 13:22:07 +0530

    Added welcome message

---
+++ MyStack.java
@@ -0,0 +1,14 @@
+package stack;
+
+import linkedlist.INode;
+
+public class MyStack {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to master Branch of stack");
+	}
+
+}
+
